09b0ab7ea7ed9cde460e5fe2f50e94cd4651e108
SailauNurbek <nurbeksailau02@gmail.com>
2023-08-12 22:43:42 +0500

    2

--- Module 10/Consultant.cs
+++ Module 10/Consultant.cs
@@ -6,13 +6,13 @@ using System.Threading.Tasks;
 
 namespace Module_10
 {
-    internal class Class1
+    internal class Consultant
     {
         private string surName = "Sailau";
         private string name = "Nurbek";
         private string patronymic = "Alibekuly";
         private string number = "+01234567890";
-        private string id = "123456789123456789";
+        private string pasportId = "123456789123456789";
 
         /// <summary>
         /// Свойство для просмотра имени
@@ -35,15 +35,30 @@ namespace Module_10
         public string Number { get { return number; } set { setNumber(); } }
 
         //Консультант может изменить «Номер телефона», но при этом поле должно быть всегда заполнено.
-        public string setNumber()
+        public void setNumber()
         {
             this.number = Console.ReadLine();
             while (this.number.Length < 11 || this.number.Length > 11 || this.number == String.Empty)
             {
-                Console.WriteLine("Не корректный номер, поле пустое. Введите номер заново");
+                Console.WriteLine("Не корректный номер или отсутсутвуют данные. Введите номер заново");
                 this.number = Console.ReadLine();
+            }            
+        }
+
+        /// <summary>
+        /// string method wich retutn Id of pasport
+        /// </summary>
+        /// <returns></returns>
+        public string showId()
+        {
+            if (pasportId == String.Empty)
+            {
+                return "Не корректный номер паспорта или отсутсутвуют данные";
             }
-            return string.Empty;
+            else
+            {
+                return"******************";
+            }
         }
     }
 }
--- Module 10/Program.cs
+++ Module 10/Program.cs
@@ -5,17 +5,24 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
-            Class1 person = new Class1();
+            Consultant person = new Consultant();
 
             Console.WriteLine("Name:" + person.Name);
             Console.WriteLine("Sur name:" + person.SurName);
             Console.WriteLine("Patronymic:" + person.Patronymic);
             Console.WriteLine("Number:" + person.Number);
+            Console.WriteLine("Pasport Id: " + person.showId());
 
             Console.WriteLine();
             Console.WriteLine("Change a new number");
             person.setNumber();
             Console.WriteLine("New number:" + person.Number);
+            
+
+            Console.WriteLine();
+            Manager manager = new Manager();
+            Console.WriteLine("Pasport Id: " + manager.showId() + " Manager");
+
         }
     }
-}
+} 
